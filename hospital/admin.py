71d730ea4bf446fe3c8fbda09a7fbe2edb70693b
Xobotyara <probosciss@rambler.ru>
2019-12-27 22:51:52 +0600

    Skelet gotov

--- hospital/admin.py
+++ hospital/admin.py
@@ -4,3 +4,12 @@ from .models import *
 # Register your models here.
 admin.site.register(Hospital)
 admin.site.register(Department)
+admin.site.register(Cabinet)
+admin.site.register(Gender)
+admin.site.register(Post)
+admin.site.register(Pacient)
+admin.site.register(Medicine)
+admin.site.register(Procedure)
+admin.site.register(Diagnose)
+admin.site.register(Person)
+admin.site.register(Reciept)
